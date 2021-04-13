d614549c2ed31c8442d2acf82972b0e18f6fdd99
Fábio Alencastro Pinheiro <fabioalencastro@hotmail.com>
2021-04-13 19:10:19 -0300

    nao consegui adicionar produtos a api

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuarioModule } from './usuario/usuario.module';
+import { ProdutoModule } from './produto/produto.module';
 
 @Module({
-  imports: [UsuarioModule],
+  imports: [UsuarioModule, ProdutoModule],
   controllers: [AppController],
   providers: [AppService],
 })
